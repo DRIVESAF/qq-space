586c069a3cf9340f2a30ffae9c8f3b21d6d2718e
blackcat <123@fox.mail>
2025-01-20 16:10:02 +0800

    测试 husky

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const msg = ref<string>('hello')
+console.log('aaa')
+</script>
+
+<style scoped></style>
